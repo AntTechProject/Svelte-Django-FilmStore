222f111c82949996105eb607ea53e80e61446bf6
Nahin Alam <nahin.alam@northsouth.edu>
2024-08-20 04:43:05 +0600

    initial commit

---
+++ src/routes/films/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let films = [
+        {id: 1, name: 'The GodFather', director: 'Francis'},
+        {id: 2, name: 'The Batman', director: 'Nolan'},
+    ]
+</script>
+
+<ul>
+    {#each films as film}
+        <li><a href="films/{film.id}">{film.name}</a></li>
+    {/each}
+</ul>
